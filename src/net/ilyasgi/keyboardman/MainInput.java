d4b5c3e3b892700ffb3071323ec6e42f2ff20ea1
phoenix-gi <ilyas_gi@mail.ru>
2018-08-19 12:12:35 +0300

    Add: main input

---
+++ src/net/ilyasgi/keyboardman/MainInput.java
@@ -0,0 +1,35 @@
+package net.ilyasgi.keyboardman;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MainInput implements KeyListener {
+
+    private GameModel model;
+
+    MainInput(GameModel model) {
+        this.model = model;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+        if (model.isStart()) {
+            model.handleKey(keyEvent);
+        }
+        MainPanel.getPanel().repaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+        if (!model.isStart()) {
+            if (keyEvent.getKeyCode() == KeyEvent.VK_SPACE) {
+                model.start();
+            }
+        }
+        MainPanel.getPanel().repaint();
+    }
+}
