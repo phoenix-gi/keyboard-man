78d16ece2b14fd075a201c2f208831db52815ed1
phoenix-gi <ilyas_gi@mail.ru>
2018-08-19 10:16:22 +0300

    Add: screen interface

--- src/net/ilyasgi/keyboardman/MainPanel.java
+++ src/net/ilyasgi/keyboardman/MainPanel.java
@@ -2,12 +2,14 @@ package net.ilyasgi.keyboardman;
 
 import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Color;
 
 final class MainPanel {
     private static MainPanel instance;
 
     private JPanel panel;
+    private Screen screen;
 
     private MainPanel() {
         buildPanel();
@@ -21,6 +23,9 @@ final class MainPanel {
                 super.paint(g);
                 g.setColor(Color.WHITE);
                 g.fillRect(0, 0, getWidth(), getHeight());
+                if (screen != null) {
+                    screen.paint((Graphics2D) g);
+                }
             }
         };
     }
@@ -30,5 +35,10 @@ final class MainPanel {
             instance = new MainPanel();
         }
         return instance.panel;
+    }
+
+    static void setScreen(Screen screen) {
+        instance.screen = screen;
+        instance.panel.repaint();
     }
 }
---
+++ src/net/ilyasgi/keyboardman/Screen.java
@@ -0,0 +1,7 @@
+package net.ilyasgi.keyboardman;
+
+import java.awt.Graphics2D;
+
+public interface Screen {
+    void paint(Graphics2D g2d);
+}
