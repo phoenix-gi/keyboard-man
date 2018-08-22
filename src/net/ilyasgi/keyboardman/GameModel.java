4b544ab65c878ca11a9bb4c30414a4d07985c39c
phoenix-gi <ilyas_gi@mail.ru>
2018-08-22 19:13:51 +0300

    Update: add next, failed, success symbols to game model

--- src/net/ilyasgi/keyboardman/GameModel.java
+++ src/net/ilyasgi/keyboardman/GameModel.java
@@ -12,10 +12,19 @@ class GameModel {
     private int trueTypes;
     private int allTypes;
     private Random rnd;
+    private char nextSymbol;
+    private char[] failed;
+    private char[] success;
 
     GameModel() {
         start = false;
         rnd = new Random();
+        failed = new char[10];
+        success = new char[10];
+        for (int i = 0; i < 10; i++) {
+            failed[i] = ' ';
+            success[i] = ' ';
+        }
     }
 
     void start() {
@@ -24,6 +33,7 @@ class GameModel {
         trueTypes = 0;
         allTypes = 0;
         currentSymbol = getRandomChar();
+        nextSymbol = getRandomChar();
     }
 
     void stop() {
@@ -37,9 +47,27 @@ class GameModel {
     void handleKey(KeyEvent keyEvent) {
         if (keyEvent.getKeyChar() == currentSymbol) {
             trueTypes++;
+            for (int i = success.length - 1; i > 0; i--) {
+                success[i] = success[i - 1];
+            }
+            success[0] = currentSymbol;
+            for (char c : success) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = failed.length - 1; i > 0; i--) {
+                failed[i] = failed[i - 1];
+            }
+            failed[0] = currentSymbol;
+            for (char c : failed) {
+                System.out.print(c + " ");
+            }
+            System.out.println();
         }
         allTypes++;
-        currentSymbol = getRandomChar();
+        currentSymbol = nextSymbol;
+        nextSymbol = getRandomChar();
     }
 
     char getCurrentSymbol() {
@@ -64,5 +92,17 @@ class GameModel {
 
     private char getRandomChar() {
         return ENG_SYMBOLS.charAt(rnd.nextInt(ENG_SYMBOLS.length()));
+    }
+
+    char[] getSuccess() {
+        return success;
+    }
+
+    char[] getFailed() {
+        return failed;
+    }
+
+    char getNextSymbol() {
+        return nextSymbol;
     }
 }
